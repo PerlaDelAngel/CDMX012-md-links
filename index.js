76fc813bb5346000a09bb8979e1cdf768e4596d5
Perla Del Ángel <perdelangel@gmail.com>
2022-04-06 21:45:45 -0500

    Implementing support for a single file, can read and identify it's extention. Closes #4

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-module.exports = () => {
+/* module.exports = () => {
   // ...
-};
+}; */
+
+const fs = require('fs'); //file system
+const path = require('path'); // working with file and directory paths
+// const readline = require('readline'); // to receive user inputs
+
+// Reading a file
+fs.readFile('./doc.md', 'utf8', (err, data) => {
+  if(err){
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
+
+// Extracting the type of the file
+const ext = path.extname('./doc.md')
+console.log(ext);
