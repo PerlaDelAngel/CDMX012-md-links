9b8fa0fcbd778f3eb0f4bb726eccf80c8094d501
Perla Del Ángel <perdelangel@gmail.com>
2022-04-11 18:57:37 -0500

    Extrae los links de un archivo con su respectivo texto asociado. Closes #26

--- read-file.js
+++ read-file.js
@@ -1,22 +1,46 @@
 const fs = require('fs'); //file system
 const path = require('path'); // working with file and directory paths
-const markdownLinkExtractor = require('markdown-link-extractor');
 
-//Reading a file
-function readFile(filePath){ //recibe un archivo
+
+//Reading a file and extracting links
+function getLinks(filePath){ //recibe un archivo
   const data = fs.readFileSync(filePath, 'utf8');
-  const { links } = markdownLinkExtractor(data, true); 
-  return links;
+
+  const textRegExp = /\[(.*)\]/g;
+  let titles = []
+  const linkTitles = [...data.matchAll(textRegExp)];
+  linkTitles.forEach(linkTitle => {
+    titles.push(linkTitle[1])
+    //console.log(linkTitle[1]); 
+  });
+  
+  const linkRegExp = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/g;
+  let links = [];
+  const linkMatches = [...data.matchAll(linkRegExp)]
+  linkMatches.forEach(link => {
+    links.push(link[0])
+    //console.log(link[0]); 
+  });
+
+  const linksObj = {};
+  titles.forEach((title, index) => {
+    linksObj[title] = links[index];
+  })
+  //console.log(linksObj)
+  
+  return linksObj;
 }
 
 //Function for the array that contains all the md file paths
-function readFilesRec(filesArr){
-  filesArr.forEach(mdFile => {
-    const links = readFile(mdFile);
+function defaultBehavior(filesArr){
+  filesArr.forEach(mdFile => { //Recorre los archivos
+    const links = getLinks(mdFile); //Extrae links
     //console.log(links);
-    links.forEach(link => console.log(`${link.href} => ${link.text}`))
+    links.forEach(link => { //Recorre los links
+      //console.log(`${mdFile} ${link}`)
+    }) 
   })
 }
 
 
-module.exports = {readFilesRec, readFile}
+module.exports = {defaultBehavior, getLinks}
