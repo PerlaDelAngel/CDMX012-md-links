fcbf8d72ea91fe61cfd5827f81dcd8542c743c16
Perla Del Ángel <perdelangel@gmail.com>
2022-04-16 16:51:42 -0500

    Implements combined use of stats and validate. Closes #34

--- stats.js
+++ stats.js
@@ -1,6 +1,21 @@
+const validate = require('./link-validation');
+const { resolvePath } = require('./validate-path');
+
 function stats(linksObj){
   let unique = [...new Set(linksObj.map(item => item.href))];
   process.stdout.write(`Total: ${linksObj.length}\nUnique: ${unique.length}`);
 }
 
-module.exports = {stats}
+function combinedStats(linksObj){
+  let unique = [...new Set(linksObj.map(item => item.href))];
+
+  const validateLinks = linksObj.map(link => validate.validateLink(link));
+
+  Promise.all(validateLinks)
+  .then((result)=>{
+    const broken = result.filter(link => link.status >= 400);
+    process.stdout.write(`Total: ${linksObj.length}\nUnique: ${unique.length}\nBroken: ${broken.length}`);
+  }) 
+}
+
+module.exports = {stats, combinedStats}
