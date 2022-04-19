3b05a65a37eb60fe17ca2865313b6cce5d0fd749
Perla Del Ángel <perdelangel@gmail.com>
2022-04-19 10:16:19 -0500

    Tests for the read-file file

---
+++ test/read-file.spec.js
@@ -0,0 +1,23 @@
+const pathIsFile = require('../lib/read-file');
+
+describe('Identifies markdown files', () => {
+  it('should receive a path to a file that is not markdown', () => {
+    let mdFiles = [];
+    const absolutePath = 'C:\\Users\\user\\Desktop\\Laboratoria\\ProyectosLab\\CDMX012-md-links\\test-docs\\second-dir\\testdoc2.txt';
+    const consoleSpy = jest.spyOn(console, 'log');
+
+    pathIsFile(absolutePath, mdFiles);
+
+    expect(consoleSpy).toHaveBeenCalledWith('This is not a markdown file and cannot be analyzed');
+  });
+
+  it('should receive a path to a markdown file and store it in an array', () => {
+    let mdFiles = [];
+    const absolutePath = 'C:\\Users\\user\\Desktop\\Laboratoria\\ProyectosLab\\CDMX012-md-links\\test-docs\\doc2.md';
+
+    pathIsFile(absolutePath, mdFiles);
+    
+    expect(mdFiles).toEqual(['C:\\Users\\user\\Desktop\\Laboratoria\\ProyectosLab\\CDMX012-md-links\\test-docs\\doc2.md']);
+  });
+});
+
