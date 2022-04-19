71991a65fbbfbda69951926533d131edcabda03e
Perla Del Ángel <perdelangel@gmail.com>
2022-04-19 09:55:02 -0500

    Tests for resolve-path file

--- bin/cli.js
+++ bin/cli.js
@@ -2,7 +2,7 @@
 const yargs = require('yargs');
 const chalk = require('chalk');
 const fs = require('fs');
-const mdLinks = require('../lib/index.js');
+const mdLinks = require('../lib/mdLinks.js');
 const linkStats = require('../lib/stats.js');
 
 // Styles
@@ -15,7 +15,7 @@ const emphasis = chalk.magenta;
 //Arguments 
 const argv = yargs
   .scriptName('mdLinks')
-  .usage(`\nUsage: $0 path --validate --stats \n(validate and stats commands are optional, paths can be absolute or relative`)
+  .usage(emphasis(`\nUsage: $0 path --validate --stats \n(validate and stats commands are optional, paths can be absolute or relative`))
   .example('$0 docs\\doc.md --validate --stats')
   .option('validate', {
     alias: 'v',
---
+++ test/resolve-path.spec.js
@@ -0,0 +1,12 @@
+const resolvePath = require('../lib/resolve-path');
+
+describe('Returns an absolute path', () => {
+  it('should receive a relative path and turn it into an absolute one', () => {
+    const path = resolvePath('./test-docs/doc2.md')
+    expect(path).toBe('C:\\Users\\user\\Desktop\\Laboratoria\\ProyectosLab\\CDMX012-md-links\\test-docs\\doc2.md');
+  });
+  it('should receive an absolute path and return it as is', () => {
+    const path = resolvePath('C:\\Users\\user\\Desktop\\Laboratoria\\ProyectosLab\\CDMX012-md-links\\test-docs\\doc2.md')
+    expect(path).toBe('C:\\Users\\user\\Desktop\\Laboratoria\\ProyectosLab\\CDMX012-md-links\\test-docs\\doc2.md');
+  });
+});
